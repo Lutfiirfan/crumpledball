0e31448338b19bb7ac466fea9f9a2bbd7879c60c
Lutfi Irfan <lutfibinirfan@gmail.com>
2021-06-02 19:49:43 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -8,10 +8,17 @@ class Dustbin{
        this.height = height;
        World.add(world,this.body);
     };
-    display(){
-        var pos =this.body.position;
-        rectMode(CENTER);
-        fill(255, 255, 255, 0.5);
-        rect(pos.x, pos.y, this.width, this.height);
+    display()
+    {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push()
+	translate(pos.x, pos.y);
+	rectMode(CENTER)
+        fill("white");
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,33,33);
+        pull()
       }
 }
